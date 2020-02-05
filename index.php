faf61f213d695d670e49aba52d304cae00088673
shweta_sim <shweta.sim@gmail.com>
2020-02-05 13:04:18 +0100

    sidebar updated

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-<!-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> -->
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="">
 <meta name="format-detection" content="telephone=no">
@@ -19,6 +17,9 @@
 <!-- Custom CSS -->
 <link href="css/style.css" rel="stylesheet" type="text/css">
 
+<!-- Responsive CSS -->
+<link href="css/responsive.css" rel="stylesheet" type="text/css">
+
 <!-- Animate CSS -->
 <link rel="stylesheet" href="plugins/animate/animate.min.css"/>
 </head>
@@ -26,48 +27,8 @@
 <!-- ########## Wrapper Start ########## -->
 <div class="wrapper"> 
   <!-- ########## Sidebar Start ########## -->
-  <nav id="sidebar">
-    <div class="btnCollapse"> <i></i> </div>
-    <div class="sidebar-content">
-      <h6 class="text-blue text-uppercase text-semibold mb1">CBM Global</h6>
-      <ul class="nav flex-column mb3">
-        <li class="nav-item"> <a class="nav-link" href="step1-registration-1.php"> <i><img src="imgs/icon-logo.svg"></i>
-          <div><span>Step1</span>Registration</div>
-          </a> </li>
-        <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-cb-license.png"></i>
-          <div><span>Step2</span>Buy Cashback Licenses</div>
-          </a> </li>
-        <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-select-product.svg"></i>
-          <div><span>Step3</span>Select Product</div>
-          </a> </li>
-        <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-checkout.svg"></i>
-          <div><span>Step4</span>Checkout</div>
-          </a> </li>
-      </ul>
-      <h6 class="text-blue text-uppercase text-semibold mb1">Setup Trading</h6>
-      <ul class="nav flex-column">
-        <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-registration.svg"></i>
-          <div><span>Step1</span>Registration</div>
-          </a> </li>
-        <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-kyc.svg"></i>
-          <div><span>Step2</span>KYC</div>
-          </a> </li>
-        <li class="nav-item"> <span class="text-sm note">You will receive your account number</span> </li>
-      </ul>
-      <ul class="nav flex-column">
-        <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-signing-lpoa.svg"></i>
-          <div><span>Step3</span>Signing the LPOA</div>
-          </a> </li>
-        <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-funding-account.svg"></i>
-          <div><span>Step4</span>Funding Your Account</div>
-          </a> </li>
-        <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-account-activation.svg"></i>
-          <div><span>Step5</span>Account Activation</div>
-          </a> </li>
-      </ul>
-    </div>
-  </nav>
-  <!-- ########## Sidebar End ########## -->
+  <?php include("sidebar.php"); ?>
+  <!-- ########## Sidebar End ########## --> 
   <!-- ########## Page Content Start ########## -->
   <div id="content">
     <div class="content-inner">
@@ -75,22 +36,22 @@
       <div class="container">
         <div class="row mb9 mb6-md">
           <div class="col-md-12 text-center">
-            <div class="logo mb6 mb4-lg wow fadeInUp" data-wow-delay="100ms"><a href="index.php"><img src="imgs/logo.svg" class="img-fluid"></a></div>
-            <h4 class="text-blue text-semibold text-center text-uppercase wow fadeInUp" data-wow-delay="200ms">Type of Account</h4>
+            <div class="logo mb3 wow fadeInUp" data-wow-delay="100ms"><a href="index.php"><img src="imgs/logo.svg" class="img-fluid"></a></div>
+            <h5 class="text-blue text-semibold text-center text-uppercase wow fadeInUp" data-wow-delay="200ms">Type of Account</h5>
           </div>
         </div>
         <div class="account-type" data-toggle="buttons">
           <div class="row">
             <div class="col-md-6 col-lg-6 col-xl-4 offset-xl-2 mb9-md wow fadeInLeft" data-wow-delay="300ms">
               <div class="btn"> <i><img src="imgs/icon-personal-account.svg"></i>
-                <h5 class="mb1">Personal Account</h5>
+                <h6 class="mb1">Personal Account</h6>
                 <p>You are one individual registering for one account.</p>
                 <input type="radio" name="speed">
               </div>
             </div>
             <div class="col-md-6 col-lg-6 col-xl-4 mb4-md wow fadeInRight" data-wow-delay="300ms">
               <div class="btn"> <i><img src="imgs/icon-corporate-account.svg"></i>
-                <h5 class="mb1">Corporate Account</h5>
+                <h6 class="mb1">Corporate Account</h6>
                 <p>You want to register under a business name.</p>
                 <input type="radio" name="speed" checked>
               </div>
@@ -100,7 +61,7 @@
       </div>
     </div>
   </div>
-  <!-- ########## Page Content End ########## -->
+  <!-- ########## Page Content End ########## --> 
 </div>
 <!-- ########## Wrapper End ########## --> 
 
@@ -119,7 +80,7 @@
 <!-- SVG Fill Script --> 
 <script type="text/javascript" src="plugins/js/svg-img.js"></script> 
 
-<!-- Sidebar Toggle Script -->
+<!-- Sidebar Toggle Script --> 
 <script type="text/javascript">
   $(document).ready(function () {
     $('.btnCollapse').on('click', function () {
@@ -128,5 +89,19 @@
   });
 </script>
 
+<!-- Accordion Script --> 
+<script type="text/javascript">
+       $('.panel-collapse').on('show.bs.collapse', function () {
+    $(this).siblings('.panel-heading').addClass('active');
+  });
+
+  $('.panel-collapse').on('hide.bs.collapse', function () {
+    $(this).siblings('.panel-heading').removeClass('active');
+  });
+
+  // $('[id^=accordion]').on('show.bs.collapse', function (e) {
+  // $('[id^=accordion] .panel-collapse.collapse').not(e.target).collapse('hide');
+  // });
+</script>
 </body>
 </html>
---
+++ sidebar.php
@@ -0,0 +1,65 @@
+<nav id="sidebar">
+  <div class="btnCollapse"> <i></i> </div>
+  <div class="sidebar-content"> 
+    
+    <!-- accordion start -->
+    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true"> 
+      <!-- accordion panel 1 start -->
+      <div class="panel panel-default">
+        <div class="panel-heading active" role="tab" id="headingOne">
+          <h6 class="panel-title text-blue text-uppercase text-semibold"> <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">CBM Global</a> </h6>
+        </div>
+        <div id="collapseOne" class="panel-collapse collapse show" role="tabpanel" aria-labelledby="headingOne">
+          <div class="panel-body">
+            <ul class="nav flex-column mb2">
+              <li class="nav-item"> <a class="nav-link active" href="step1-registration-1.php"> <i><img src="imgs/icon-logo.svg"></i>
+                <div><span>Step1</span>Registration</div>
+                </a> </li>
+              <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-cb-license.png"></i>
+                <div><span>Step2</span>Buy Cashback Licenses</div>
+                </a> </li>
+              <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-select-product.svg"></i>
+                <div><span>Step3</span>Select Product</div>
+                </a> </li>
+              <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-checkout.svg"></i>
+                <div><span>Step4</span>Checkout</div>
+                </a> </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- accordion panel 1 end --> 
+      <!-- accordion panel 2 start -->
+      <div class="panel panel-default">
+        <div class="panel-heading active" role="tab" id="headingTwo">
+          <h6 class="panel-title text-blue text-uppercase text-semibold mb1"> <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">Setup Trading</a> </h6>
+        </div>
+        <div id="collapseTwo" class="panel-collapse collapse show" role="tabpanel" aria-labelledby="headingTwo">
+          <div class="panel-body">
+            <ul class="nav flex-column">
+              <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-registration.svg"></i>
+                <div><span>Step1</span>Registration</div>
+                </a> </li>
+              <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-kyc.svg"></i>
+                <div><span>Step2</span>KYC</div>
+                </a> </li>
+              <li class="nav-item"> <span class="text-sm note">You will receive your account number</span> </li>
+              <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-signing-lpoa.svg"></i>
+                <div><span>Step3</span>Signing the LPOA</div>
+                </a> </li>
+              <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-funding-account.svg"></i>
+                <div><span>Step4</span>Funding Your Account</div>
+                </a> </li>
+              <li class="nav-item"> <a class="nav-link" href="#"> <i><img src="imgs/icon-account-activation.svg"></i>
+                <div><span>Step5</span>Account Activation</div>
+                </a> </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- accordion panel 2 end --> 
+    </div>
+    <!-- accordion end --> 
+    
+  </div>
+</nav>
